c3333e1a6a5df1784b607e46d6aa7a59378cf354
geovaniorsoli <112351284+geovaniorsoli@users.noreply.github.com>
2023-10-16 20:12:38 -0300

    Organização de layout e modal

--- js.js
+++ js.js
@@ -11,75 +11,72 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-
-//salvar local
 function attachEventListeners() {
-    document.getElementById('taskContainer').addEventListener('click', function(event) {
-        if (event.target.classList.contains('fas') || event.target.classList.contains('botao')) {
-            const button = event.target.closest('button');
-            const taskDiv = button.closest('.d-flex');
-            if (button.classList.contains('btn-outline-danger')) {
-                taskDiv.remove(); 
-            } else if (button.classList.contains('btn-outline-success')) {
-                taskDiv.querySelector('table').style.textDecoration = 'line-through'; // Marca como concluído.
+    const taskContainer = document.getElementById('taskContainer');
+    taskContainer.addEventListener('click', function(event) {
+        const button = event.target.closest('button');
+        if (button) {
+            const taskDiv = button.closest('.taskDiv');
+            if (button.classList.contains('btn-remove-task')) {
+                taskDiv.remove();
+                saveTasksAndTitles();
+            } else if (button.classList.contains('btn-mark-task')) {
+                taskDiv.querySelector('.taskContent').style.textDecoration = 'line-through';
                 button.disabled = true;
+                saveTasksAndTitles();
             }
-            saveTasksAndTitles(); 
         }
     });
 }
-function reattachEvents() {
-    document.querySelectorAll('.taskContainer').forEach(container => {
-        container.addEventListener('click', function(event) {
-            if (event.target.classList.contains('btn-remove-task')) {
-                event.target.closest('.taskDiv').remove();
-                saveTasksAndTitles(); 
-            } else if (event.target.classList.contains('btn-mark-task')) {
-                const taskDiv = event.target.closest('.taskDiv');
-                taskDiv.querySelector('.taskContent').style.textDecoration = 'line-through';
-                event.target.disabled = true;
-                saveTasksAndTitles(); 
-            }
-        });
-    });
-}
 
-//criar titulo
 function addTitle() {
     const titleInput = document.getElementById('titleInput');
+    if (!titleInput.value.trim()) return;
+
+    const normalizedTitleValue = titleInput.value.trim().replace(/\s+/g, '-');
     const titleSelect = document.getElementById('titleSelect');
+    const mainContainer = document.getElementById('taskContainer');
+    const taskListDiv = createTaskList(normalizedTitleValue);
+    const titleDiv = createTitleDiv(titleInput.value, normalizedTitleValue);
+
+    mainContainer.appendChild(titleDiv);
+    mainContainer.appendChild(taskListDiv);
 
     const option = document.createElement('option');
     option.value = titleInput.value;
     option.textContent = titleInput.value;
     titleSelect.appendChild(option);
 
-    const titleDiv = document.createElement('div');
-    titleDiv.className = 'row align-items-center d-flex';
-
-    
-    const normalizedTitleValue = titleInput.value.replace(/\s+/g, '-');
-    titleDiv.id = `title-${normalizedTitleValue}`;
-
-    const h3 = document.createElement('h3');
-    h3.textContent = titleInput.value;
-    titleDiv.appendChild(h3);
-
-    // Criar o botão de deletar com ícone
-    const deleteButton = document.createElement('button');
-    deleteButton.className = 'btn btn-danger btn-sm ';
-    deleteButton.innerHTML = '<i class="fas fa-trash-alt"></i>'; 
-    deleteButton.onclick = function() {
-        deleteTitle(normalizedTitleValue); 
-    };
-    titleDiv.appendChild(deleteButton);
-
-    document.getElementById('taskContainer').appendChild(titleDiv);
-
     titleInput.value = '';
     saveTasksAndTitles();
 }
 
+function createTaskList(normalizedTitleValue) {
+    const taskListDiv = document.createElement('div');
+    taskListDiv.className = 'task-list-container';
+    taskListDiv.id = `tasks-for-title-${normalizedTitleValue}`;
+    return taskListDiv;
+}
+
+function createTitleDiv(titleValue, normalizedTitleValue) {
+    const titleDiv = document.createElement('div');
+    titleDiv.className = 'gap-3 title-container align-items-end d-flex';
+    titleDiv.id = `title-${normalizedTitleValue}`;
+
+    const h3 = document.createElement('h3');
+    h3.textContent = titleValue;
+    titleDiv.appendChild(h3);
+
+    const deleteButton = document.createElement('button');
+    deleteButton.className = 'btn btn-outline-danger';
+    deleteButton.innerHTML = '<i class="fas fa-trash-alt"></i>';
+    deleteButton.addEventListener('click', function() {
+        deleteTitle(normalizedTitleValue);
+    });
+    titleDiv.appendChild(deleteButton);
+
+    return titleDiv;
+}
 
 function deleteTitle(normalizedTitleName) {
     const titleSelect = document.getElementById('titleSelect');
@@ -91,112 +88,64 @@ function deleteTitle(normalizedTitleName) {
     }
 
     const titleDiv = document.getElementById(`title-${normalizedTitleName}`);
-    if (titleDiv) {
-        titleDiv.remove();
-    }
+    const taskListDiv = document.getElementById(`tasks-for-title-${normalizedTitleName}`);
+    if (titleDiv) titleDiv.remove();
+    if (taskListDiv) taskListDiv.remove();
 
     saveTasksAndTitles();
 }
 
-//validacao 
-function validateAndAddTitle() {
-    const titleInput = document.getElementById('titleInput');
-    const titleInputError = document.getElementById('titleInputError');
-
-    // Limpando erros anteriores
-    titleInputError.style.display = 'none';
-
-    // Verificando se o input está vazio
-    if (titleInput.value.trim() === '') {
-        titleInputError.style.display = 'block';
-        return;
-    }
-
-    // Se passar na validação, adicione o título e feche o modal
-    addTitle(); // esta é a sua função que adiciona o título
-    $('#titleModal').modal('hide');
-
-    $('#titleModal').on('hidden.bs.modal', function () {
-        // Limpar o campo de entrada e a mensagem de erro quando o modal for fechado
-        document.getElementById('titleInput').value = '';
-        document.getElementById('titleInputError').style.display = 'none';
-    })
-    
-    document.getElementById('taskContainer').addEventListener('click', function(event) {
-        if (event.target.matches('.btn-danger')) { // Se o botão de deletar foi clicado.
-            const titleContainer = event.target.closest('.title-container');
-            if (titleContainer) {
-                const titleName = titleContainer.querySelector('h3').textContent;
-                deleteTitle(titleName);
-            }
-        }
-    });
-    
-}
-
-// Função para deletar um título
-function deleteTitle(titleName) {
-    // Remover do select
-    
-    const titleSelect = document.getElementById('titleSelect');
-    for (let i = 0; i < titleSelect.options.length; i++) {
-        if (titleSelect.options[i].value === titleName) {
-            titleSelect.remove(i);
-            break;
-        }
-    }
-
-    const titleDiv = document.getElementById(`title-${titleName.replace(/\s+/g, '-')}`);
-    if (titleDiv) {
-        titleDiv.remove();
-    }
-
-    saveTasksAndTitles();
-}
-
-//add tarefas
 function addTask() {
     const taskInput = document.getElementById('taskInput');
     const titleSelect = document.getElementById('titleSelect');
-    const selectedTitle = titleSelect.options[titleSelect.selectedIndex].value;
-
+    const selectedTitle = titleSelect.value;
     const normalizedTitleValue = selectedTitle.replace(/\s+/g, '-');
 
-    const titleDiv = document.getElementById(`title-${normalizedTitleValue}`);
-
-    if (taskInput.value.trim() && titleDiv) {
-        const taskDiv = document.createElement('div');
-        taskDiv.className = 'd-flex align-items-start mb-3 taskDiv';
-
-        const taskContent = document.createElement('div');
-        taskContent.className = 'taskContent';
-        taskContent.textContent = taskInput.value;
-
-        const markAsDoneButton = document.createElement('button');
-        markAsDoneButton.className = 'btn btn-outline-success mr-2 botao btn-mark-task';
-        markAsDoneButton.innerHTML = '<i class="fas fa-check-circle"></i>';
-
-        const removeButton = document.createElement('button');
-        removeButton.className = 'btn btn-outline-danger botao btn-remove-task';
-        removeButton.innerHTML = '<i class="fas fa-trash"></i>';
-
-        taskDiv.appendChild(taskContent);
-        taskDiv.appendChild(markAsDoneButton);
-        taskDiv.appendChild(removeButton);
-
-        titleDiv.appendChild(taskDiv);
-
-        taskInput.value = '';
-        $('#taskModal').modal('hide');
-
-        saveTasksAndTitles();
-    } else if (!titleDiv) {
-        // Caso não encontre o título, você pode querer alertar o usuário aqui.
-        console.error("Título não encontrado: ", selectedTitle);
+    if (taskInput.value.trim()) {
+        const taskListDiv = document.getElementById(`tasks-for-title-${normalizedTitleValue}`);
+        if (taskListDiv) {
+            const taskDiv = createTaskDiv(taskInput.value);
+            taskListDiv.appendChild(taskDiv);
+            taskInput.value = '';
+            saveTasksAndTitles();
+        }
     }
 }
+function createTaskDiv(taskContentValue) {
+    // Criando o contêiner principal para a tarefa
+    const taskDiv = document.createElement('div');
+    taskDiv.className = 'd-flex align-items-center justify-content-between taskDiv'; // Usando flexbox para alinhamento
 
-// Salvar dados
+    // Criando o div do conteúdo da tarefa
+    const taskContent = document.createElement('div');
+    taskContent.className = 'taskContent flex-grow-1'; // Permite que o conteúdo cresça para ocupar o espaço disponível
+    taskContent.textContent = taskContentValue;
+    taskDiv.appendChild(taskContent);
+
+    // Criando o botão para marcar a tarefa como concluída
+    const markAsDoneButton = document.createElement('button');
+    markAsDoneButton.className = 'btn btn-outline-success mr-2 botao btn-mark-task'; // 'mr-2' para margem à direita
+    markAsDoneButton.innerHTML = '<i class="fas fa-check-circle"></i>';
+    taskDiv.appendChild(markAsDoneButton);
+
+    // Criando o botão para remover a tarefa
+    const removeButton = document.createElement('button');
+    removeButton.className = 'btn btn-outline-danger botao btn-remove-task';
+    removeButton.innerHTML = '<i class="fas fa-trash"></i>';
+    taskDiv.appendChild(removeButton);
+
+    // Criando a linha horizontal para separação visual
+    const hr = document.createElement('hr');
+    
+    // Criando um contêiner para agrupar o taskDiv e o <hr>
+    const container = document.createElement('div');
+    container.appendChild(taskDiv);
+    container.appendChild(hr);
+
+    return container; // retornando o contêiner que inclui tanto o taskDiv quanto o <hr>
+}
+
+
 function saveTasksAndTitles() {
     const tasksContainer = document.getElementById('taskContainer');
     localStorage.setItem('tasksAndTitles', tasksContainer.innerHTML);
@@ -204,74 +153,55 @@ function saveTasksAndTitles() {
 
 function loadTasksAndTitles() {
     const tasksAndTitles = localStorage.getItem('tasksAndTitles');
-    if(tasksAndTitles) {
-        document.getElementById('taskContainer').innerHTML = tasksAndTitles;
+    if (tasksAndTitles) {
+        const tasksContainer = document.getElementById('taskContainer');
+        tasksContainer.innerHTML = tasksAndTitles;
+        attachEventListeners();  // Reanexar os event listeners após carregar o conteúdo
     }
 }
 
-// Evento para o modal
-$('#taskModal').on('hidden.bs.modal', function () {
-    document.getElementById('taskInput').value = '';
-});
-
-
-//apagartudo 
 function clearAllData() {
     localStorage.clear();
     document.getElementById('taskContainer').innerHTML = '';
-    
+    document.getElementById('titleSelect').innerHTML = ''; // Limpar as opções do select, se necessário
 }
 
-document.getElementById('clearDataButton').addEventListener('click', function() {
-    bootstrapConfirm('Quer apagar tudo?', function(result) {
-        if (result) {
-            clearAllData();
-        }
-    });
-});
-function bootstrapConfirm(message, callback) {
-    const confirmModal = `
-    <div class="modal fade" id="confirmModal" tabindex="-1" aria-labelledby="confirmModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
+
+function bootstrapConfirm(type, message, callback) {
+    // Cria o elemento modal
+    const modal = document.createElement('div');
+    modal.className = 'modal fade'; // Adiciona a classe 'fade' para animação de desvanecimento
+    modal.tabIndex = -1;
+    modal.setAttribute('aria-hidden', 'true');
+    modal.innerHTML = `
+        <div class="modal-dialog modal-dialog-centered"> <!-- 'modal-dialog-centered' para centralizar -->
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="confirmModalLabel">Confirmação</h5>
-                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal"><i class="fas fa-times"></i></button>
-                    
+                    <h5 class="modal-title">Confirmação</h5>
+                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal" aria-label="Close"> <i class="fas fa-times"></i> </button>
                 </div>
                 <div class="modal-body">
-                    ${message}
+                    <p>${message}</p>
                 </div>
                 <div class="modal-footer">
-                    
-                    <button type="button" class="btn btn-success" id="confirmYes">Sim</button>
+                    <button type="button" class="btn btn-success ${type}" id="confirmButton"> <i class="fas fa-check"></i> </button>
                 </div>
             </div>
         </div>
-    </div>`;
+    `;
 
-   
-    document.body.insertAdjacentHTML('beforeend', confirmModal);
+    document.body.appendChild(modal);
+    const modalInstance = new bootstrap.Modal(modal);
 
-    // Mostra o modal
-    const modalElement = new bootstrap.Modal(document.getElementById('confirmModal'));
-    modalElement.show();
-
-    document.getElementById('confirmYes').addEventListener('click', function() {
+    modal.querySelector('#confirmButton').addEventListener('click', function() {
         callback(true);
-        modalElement.hide(); 
+        modalInstance.hide();
     });
 
-    document.getElementById('confirmModal').addEventListener('hidden.bs.modal', function () {
-        this.remove(); 
-        document.body.classList.remove('modal-open'); 
-        document.querySelector('.modal-backdrop').remove(); 
+    modal.addEventListener('hidden.bs.modal', function (event) {
         callback(false);
+        document.body.removeChild(modal);
     });
-}
 
-// Tema
-function toggleTheme() {
-    const element = document.body;
-    element.dataset.bsTheme = element.dataset.bsTheme === 'light' ? 'dark' : 'light';
+    modalInstance.show();
 }
